b0d879937e7419801685b346793105d8af052f8e
koukimetal <comcometal@gmail.com>
2018-09-19 13:01:08 +0900

    Update and rename iframe.js to counter.js

--- yogax/counter.js
+++ yogax/counter.js
@@ -1,4 +1,2 @@
-window.document.querySelector('#hoge').innerText = window.parent.document.querySelector('#content').innerText;
-
 var count = 1;
 setInterval(function() {window.document.querySelector('#piyo').innerText = count; count++;}, 1000);
